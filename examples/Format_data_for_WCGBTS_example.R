e39d9ee64c328d0584f3e35f1a5a12273e8003f6
James-Thorson <JamesT.esq@gmail.com>
2015-04-10 11:56:46 -0700

    Revert "compressed extrapolation_data file"
    This reverts commit 68de2ab1d4224334659a648b03fea93869afe7b3.

---
+++ examples/Format_data_for_WCGBTS_example.R
@@ -0,0 +1,9 @@
+
+
+WCGBTS_Canary_example <- NWFSC_Trawl
+
+WCGBTS_Canary_example <- WCGBTS_Canary_example[,c('SPECIES','PROJECT_CYCLE','VESSEL','BEST_DEPTH_M','BEST_LAT_DD','BEST_LON_DD','AREA_SWEPT_HA','HAUL_WT_KG','PASS')]
+
+WCGBTS_Canary_example[,'VESSEL'] = paste0( "Vessel_",letters[as.numeric(WCGBTS_Canary_example[,'VESSEL'])] )
+devtools::use_data( WCGBTS_Canary_example, pkg=getwd())
+
