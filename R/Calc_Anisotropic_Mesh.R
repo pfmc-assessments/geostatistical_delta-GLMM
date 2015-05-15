2c2982796434fce0c4cc0b20dbf3487b95d3e60a
James-Thorson <JamesT.esq@gmail.com>
2015-05-15 08:09:39 -0700

    small changes for clarity

--- R/Calc_Anisotropic_Mesh.R
+++ R/Calc_Anisotropic_Mesh.R
@@ -18,10 +18,10 @@ function(loc_x, refine=FALSE, ...){
   E1 = V0 - V2
   E2 = V1 - V0
   
-  # Calculate Areas
-  TmpFn = function(Vec1,Vec2) abs(det( rbind(Vec1,Vec2) ))
+  # Calculate Areas 
+  crossprod_fn = function(Vec1,Vec2) abs(det( rbind(Vec1,Vec2) ))
   Tri_Area = rep(NA, nrow(E0))
-  for(i in 1:length(Tri_Area)) Tri_Area[i] = TmpFn( E0[i,],E1[i,] )/2   # T = area of each triangle
+  for(i in 1:length(Tri_Area)) Tri_Area[i] = crossprod_fn( E0[i,],E1[i,] )/2   # T = area of each triangle
 
   # Return stuff
   Return = list("Tri_Area"=Tri_Area, "TV"=TV, "E0"=E0, "E1"=E1, "E2"=E2, "mesh"=mesh, "spde"=spde)
--- R/Heatmap_Legend.R
+++ R/Heatmap_Legend.R
@@ -1,11 +1,13 @@
 Heatmap_Legend <-
-function( colvec, heatrange, margintext=NULL ){
+function( colvec, heatrange, margintext=NULL, labeltransform="uniform" ){
   par( xaxs="i", yaxs="i", mar=c(1,0,1,2+ifelse(is.null(margintext),0,1.5)), mgp=c(1.5,0.25,0), tck=-0.02 )
   N = length(colvec)
   Y = seq(heatrange[1], heatrange[2], length=N+1)
   plot( 1, type="n", xlim=c(0,1), ylim=heatrange, xlab="", ylab="", main="", xaxt="n", yaxt="n", cex.main=1.5)
   for( i in 1:N) polygon( x=c(0,1,1,0), y=Y[c(i,i,i+1,i+1)], col=colvec[i], border=NA)
-  axis(side=4, at=pretty(heatrange) )
+  if( labeltransform=="uniform" ) Labels = pretty(heatrange)
+  if( labeltransform=="inv_log10" ) Labels = 10^pretty(heatrange)
+  axis(side=4, at=pretty(heatrange), labels=Labels )
   if(!is.null(margintext)) mtext(side=4, text=margintext, line=2, cex=1.5)
   #mtext(side=1, outer=TRUE, line=1, "Legend")
 }
