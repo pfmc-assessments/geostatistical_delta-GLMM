12bc8085033180b3264440373758192630e94d3b
James-Thorson <JamesT.esq@gmail.com>
2015-08-17 10:01:51 -0700

    small change to plotting utility

--- R/Plot_Points_and_Bounds_Fn.R
+++ R/Plot_Points_and_Bounds_Fn.R
@@ -1,7 +1,12 @@
 
-Plot_Points_and_Bounds_Fn = function( x, y, ybounds, fn=lines, col="black", ... ){
-  fn( y=y, x=x, col=col, ... )
-  for(t in 1:length(y)){
-    lines( x=rep(x[t],2), y=ybounds[t,], col=col)
+Plot_Points_and_Bounds_Fn = function( x, y, ybounds, fn=lines, col_bounds="black", bounds_type="whiskers", ... ){
+  fn( y=y, x=x, ... )
+  if( bounds_type=="whiskers" ){
+    for(t in 1:length(y)){
+      lines( x=rep(x[t],2), y=ybounds[t,], col=col_bounds)
+    }
+  }
+  if( bounds_type=="shading" ){
+    polygon( x=c(x,rev(x)), y=c(ybounds[,1],rev(ybounds[,2])), col=col_bounds, border=NA)
   }
 }
