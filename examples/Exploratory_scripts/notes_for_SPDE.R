1669c946c20f4c6640dd76c0a85f9f5d8c1682f4
James-Thorson <JamesT.esq@gmail.com>
2015-10-19 11:15:13 -0700

    adding notes on SPDE

---
+++ examples/Exploratory_scripts/notes_for_SPDE.R
@@ -0,0 +1,16 @@
+# Lindgren et al. 2011 notation -> Rinla notation
+
+# See Eq. 21
+# C -> M0
+# G -> M1
+# G*(C^1)*G -> M2
+
+# see above Eq. 10
+# K = kappa^2*C + G -> kappa^2*M0 + M1
+
+################
+# General notes
+################
+
+# Alpha = Nu + d/2 (Lindgren et al. 2011, Eq. 2)
+
