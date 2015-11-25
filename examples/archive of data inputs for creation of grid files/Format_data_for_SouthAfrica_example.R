f189a2d8c374abf9769ebe026da6cdb734730dbf
Jim Thorson <JamesT.esq@gmail.com>
2015-11-25 07:17:14 -0800

    added South Africa case study...
    ... which restricts to West Coast catches of  javopever, with old gear,
    in the summer, from RV Africana

--- examples/archive of data inputs for creation of grid files/Format_data_for_SouthAfrica_example.R
+++ examples/archive of data inputs for creation of grid files/Format_data_for_SouthAfrica_example.R
@@ -11,3 +11,15 @@ points( x=south_africa_grid$cen_long, y=south_africa_grid$cen_lat)
 
 setwd( "C:/Users/Jim/Desktop/Project_git/geostatistical_delta-GLMM" )
 devtools::use_data( south_africa_grid, pkg=getwd())
+
+#######################
+# Prepare CPUE data
+#######################
+
+TmbDir = "C:/Users/Jim/Desktop/Project_git/geostatistical_delta-GLMM/inst/executables/"
+setwd( TmbDir )
+south_africa_westcoast_jacopever = read.csv( paste0(getwd(),"/../../examples/archive of data inputs for creation of grid files/South Africa/SAWC_geodata.csv") )
+south_africa_westcoast_jacopever = south_africa_westcoast_jacopever[which(south_africa_westcoast_jacopever$season=="Summer" & south_africa_westcoast_jacopever$vessel=="RV Africana" & south_africa_westcoast_jacopever$gear.used=="Dem_old"),]
+south_africa_westcoast_jacopever = south_africa_westcoast_jacopever[,c('Year','cen_lat','cen_long','HELDAC','area_swept_nm2','start_depth_bottom_m','season','vessel','gear.used')]
+devtools::use_data( south_africa_westcoast_jacopever, pkg=paste0(getwd(),"/../../"))
+
