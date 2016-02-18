673a6a0c0d883beb0b5cab932b0c0f501ed83b7e
Jim Thorson <JamesT.esq@gmail.com>
2016-02-18 09:21:27 -0800

    fixed bug from debugger from failed commit

--- R/Plot_Points_and_Bounds_Fn.R
+++ R/Plot_Points_and_Bounds_Fn.R
@@ -1,12 +1,6 @@
 
-<<<<<<< HEAD
 Plot_Points_and_Bounds_Fn = function( x, y, ybounds, fn=lines, col_bounds="black", bounds_type="whiskers", border=NA, border_lty="solid", ... ){
   fn( y=y, x=x, ... )
-=======
-Plot_Points_and_Bounds_Fn = function( x, y, ybounds, fn=lines, col_bounds="black", bounds_type="whiskers", ylim=NULL, ... ){
-  if( is.null(ylim)) ylim = range(ybounds)
-  fn( y=y, x=x, ylim=ylim, ... )
->>>>>>> origin/master
   if( bounds_type=="whiskers" ){
     for(t in 1:length(y)){
       lines( x=rep(x[t],2), y=ybounds[t,], col=col_bounds)
