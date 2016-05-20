1ab3474ea6dbc14d9d2c82108587b53e2024195a
Jim Thorson <JamesT.esq@gmail.com>
2016-05-20 11:05:27 -0700

    added NS-IBTS sample locations...
    ... to show on "Global Coverage" figure

--- examples/Surveys_available.R
+++ examples/Surveys_available.R
@@ -14,27 +14,32 @@ png( file="C:/Users/James.Thorson/Desktop/Project_git/geostatistical_delta-GLMM/
   title( "Global data coverage" )
 
   # Get extrapolation data
-  for(i in 1:9 ){
+  for(i in 1:11 ){
     if(i==1) Extrapolation_List = Prepare_WCGBTS_Extrapolation_Data_Fn( strata.limits=data.frame('STRATA'="All_areas") )
     if(i==2) Extrapolation_List = Prepare_BC_Coast_Extrapolation_Data_Fn( strata.limits=data.frame('STRATA'="All_areas"), strata_to_use=c("SOG","WCVI","QCS","HS","WCHG") )
     if(i==3) Extrapolation_List = Prepare_EBS_Extrapolation_Data_Fn( strata.limits=data.frame('STRATA'="All_areas") )
     if(i==4) Extrapolation_List = Prepare_GOA_Extrapolation_Data_Fn( strata.limits=data.frame('STRATA'="All_areas") )
     if(i==5) Extrapolation_List = Prepare_NWA_Extrapolation_Data_Fn( strata.limits=list('All_areas'=1:1e5) )
-    if(i==6) Extrapolation_List = Prepare_SA_Extrapolation_Data_Fn( strata.limits=data.frame('STRATA'="All_areas"), region=c("south_coast","west_coast") )
-    if(i==7){
+    if(i==6) Extrapolation_List = Prepare_SA_Extrapolation_Data_Fn( strata.limits=data.frame('STRATA'="All_areas"), region="west_coast" )
+    if(i==7) Extrapolation_List = Prepare_SA_Extrapolation_Data_Fn( strata.limits=data.frame('STRATA'="All_areas"), region="south_coast" )
+    if(i==8){
       data( iceland_cod )
       Data_Geostat = data.frame( "Catch_KG"=iceland_cod[,'Catch_b'], "Year"=iceland_cod[,'year'], "Vessel"=1, "AreaSwept_km2"=iceland_cod[,'towlength'], "Lat"=iceland_cod[,'lat1'], "Lon"=iceland_cod[,'lon1'])
       Data_Geostat = na.omit( Data_Geostat )
       Extrapolation_List = Prepare_Other_Extrapolation_Data_Fn( strata.limits=data.frame('STRATA'="All_areas"), observations_LL=Data_Geostat[,c('Lat','Lon')], maximum_distance_from_sample=15 )
     }
-    if(i==8) Extrapolation_List = Prepare_GSL_Extrapolation_Data_Fn( strata.limits=data.frame('STRATA'="All_areas") )
-    if(i==9){
+    if(i==9) Extrapolation_List = Prepare_GSL_Extrapolation_Data_Fn( strata.limits=data.frame('STRATA'="All_areas") )
+    if(i==10){
       load( "C:/Users/James.Thorson/Desktop/Project_git/geostatistical_delta-GLMM/data/chatham_rise_grid.rda" )
       Extrapolation_List = Prepare_NZ_Extrapolation_Data_Fn( strata.limits=data.frame('STRATA'="All_areas"), Data=chatham_rise_grid )
+    }
+    if(i==11){
+      load( "C:/Users/James.Thorson/Desktop/Project_git/geostatistical_delta-GLMM/data/north_sea_IBTS_sample_locations.rda" )
+      Extrapolation_List = Prepare_Other_Extrapolation_Data_Fn( strata.limits=data.frame('STRATA'="All_areas"), maximum_distance_from_sample=25, grid_dim_km=c(10,10), observations_LL=north_sea_IBTS_sample_locations[,c("Lon","Lat")] )
     }
 
     # Plot
     Which = which( Extrapolation_List$a_el>0 )     # 1:nrow(Extrapolation_List$a_el)
-    points( Extrapolation_List$Data_Extrap[Which,c('Lon','Lat')], pch=20, cex=0.01, col=rainbow(9)[i])
+    points( Extrapolation_List$Data_Extrap[Which,c('Lon','Lat')], pch=20, cex=0.01, col=rainbow(11)[i])
   }
 dev.off()
