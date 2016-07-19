cbfcef9e05a803515dfba01895294f74dd80a619
Jim Thorson <JamesT.esq@gmail.com>
2016-07-19 10:44:48 -0700

    added Aleutian Islands sampling grid

---
+++ examples/archive of data inputs for creation of grid files/Format_data_for_Aleutian_Islands_example.R
@@ -0,0 +1,28 @@
+
+setwd("C:/Users/James.Thorson/Desktop/Project_git/geostatistical_delta-GLMM/")
+library(mapdata)
+
+# read file
+aleutian_islands_grid = foreign::read.dbf( "examples/archive of data inputs for creation of grid files/aigrid2nmx2nm.dbf" )
+
+# Remove unnecessary columns
+aleutian_islands_grid = ThorsonUtilities::rename_columns( aleutian_islands_grid[,c('POINT_X','POINT_Y','STRATUM')], newname=c("Lon","Lat","Stratum"))
+aleutian_islands_grid = cbind( aleutian_islands_grid, "Area_km2"=4 * 1.852^2)
+
+# Plot
+Which = which( aleutian_islands_grid[,'Stratum']!=0 )
+maps::map( "worldHires" )
+points( y=aleutian_islands_grid[Which,'Lat'], x=aleutian_islands_grid[Which,'Lon'])
+
+# Convert previous TXT format to new RDA format
+devtools::use_data( aleutian_islands_grid, pkg=getwd())
+
+##############
+# Check areas
+##############
+
+# Total
+sum( aleutian_islands_grid[which( aleutian_islands_grid[,'Stratum']!=0 ), "Area_km2"] )
+
+# By stratum
+tapply( INDEX=aleutian_islands_grid$Stratum, FUN=sum, X=aleutian_islands_grid$Area_km2 )
