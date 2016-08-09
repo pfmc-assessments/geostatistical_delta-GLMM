67fd3b450cfb6cac52b03779796f0b6412d20081
Jim Thorson <JamesT.esq@gmail.com>
2016-08-09 15:56:22 -0700

    added dependency on 'utils'

--- R/zzz.R
+++ R/zzz.R
@@ -5,15 +5,15 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("Loading package SpatialDeltaGLMM, developed by James Thorson for the Northwest Fisheries Science Center")
   packageStartupMessage("For details and citation guidance, please see https://github.com/nwfsc-assess/geostatistical_delta-GLMM/#description-of-package")
-  if( !"INLA" %in% installed.packages()[,1] ){
+  if( !"INLA" %in% utils::installed.packages()[,1] ){
     message("Installing INLA...")
-    install.packages("INLA", repos="https://www.math.ntnu.no/inla/R/stable")  
+    utils::install.packages("INLA", repos="https://www.math.ntnu.no/inla/R/stable")
   }
-  #if( !"TMB" %in% installed.packages()[,1] ){
+  #if( !"TMB" %in% utils::installed.packages()[,1] ){
   #  message("Installing TMB...")
   #  devtools::install_github("kaskr/adcomp/TMB")
   #}
-  if( !"TMBhelper" %in% installed.packages()[,1] ){
+  if( !"TMBhelper" %in% utils::installed.packages()[,1] ){
     print("Installing package: TMBhelper...")
     devtools::install_github("kaskr/TMB_contrib_R/TMBhelper")
   }
