0868d455ca5e3ff601caae1d6b58a42a68a81c4d
Jim Thorson <JamesT.esq@gmail.com>
2016-10-22 14:43:15 -0700

    small fixes to Plot_data_and_knots

--- R/Plot_data_and_knots.R
+++ R/Plot_data_and_knots.R
@@ -13,14 +13,20 @@
 #'
 
 #' @export
-Plot_data_and_knots = function( Extrapolation_List, Spatial_List, Data_Geostat, PlotDir=paste0(getwd(),"/"), Plot1_name="Data_and_knots.png", Data_name="Data_by_year.png", ...){
+Plot_data_and_knots = function( Extrapolation_List, Spatial_List, Data_Geostat, PlotDir=paste0(getwd(),"/"), Plot1_name="Data_and_knots.png", Plot2_name="Data_by_year.png", ...){
+
+  # avoid attaching maps and mapdata to use worldHires plotting
+  require(maps)
+  require(mapdata)
+  on.exit( detach("package:mapdata") )
+  on.exit( detach("package:maps"), add=TRUE )
 
   # Plot data and grid
   png( file=paste0(PlotDir,Plot1_name), width=6, height=6, res=200, units="in")
     par( mfrow=c(2,2), mar=c(3,3,2,0), mgp=c(1.75,0.25,0) )
-    plot( Extrapolation_List$Data_Extrap[which(Extrapolation_List$Extrap_Area_km2>0),c('Lon','Lat')], cex=0.01, main="Extrapolation (UTM)" )
-    maps::map( "world", add=TRUE )
-    plot( Extrapolation_List$Data_Extrap[which(Extrapolation_List$Extrap_Area_km2>0),c('E_km','N_km')], cex=0.01, main="Extrapolation (North-East)" )
+    plot( Extrapolation_List$Data_Extrap[which(Extrapolation_List$Area_km2_x>0),c('Lon','Lat')], cex=0.01, main="Extrapolation (UTM)" )
+    map( "world", add=TRUE )
+    plot( Extrapolation_List$Data_Extrap[which(Extrapolation_List$Area_km2_x>0),c('E_km','N_km')], cex=0.01, main="Extrapolation (North-East)" )
     plot( Spatial_List$loc_x, col="red", pch=20, main="Knots (North-East)")
     plot( Data_Geostat[,c('E_km','N_km')], col="blue", pch=20, cex=0.1, main="Data (North-East)")
   dev.off()
@@ -36,7 +42,7 @@ Plot_data_and_knots = function( Extrapolation_List, Spatial_List, Data_Geostat, 
     for( t in 1:length(Year_Set) ){
       Which = which( Data_Geostat$Year == Year_Set[t] )
       plot( x=Data_Geostat[Which,'Lon'], y=Data_Geostat[Which,'Lat'], cex=0.01, main=Year_Set[t], xlim=range(Data_Geostat[,'Lon']), ylim=range(Data_Geostat[,'Lat']), xaxt="n", yaxt="n", ... )
-      maps::map( "world", add=TRUE )
+      map( "world", add=TRUE )
       if( t>(length(Year_Set)-Ncol) ) axis(1)
       if( t%%Ncol == 1 ) axis(2)
       mtext( side=c(1,2), text=c("Longitude","Latitude"), outer=TRUE, line=1)
