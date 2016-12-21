45a64cf5e2e7fc976fc10097f94d4ade83bca68f
Jim Thorson <JamesT.esq@gmail.com>
2016-12-20 20:33:11 -0800

    Updated `Geostat_Sim` ...
    ... to add known trend in abundance

--- R/Geostat_Sim.R
+++ R/Geostat_Sim.R
@@ -11,8 +11,10 @@ function(Sim_Settings, Extrapolation_List, MakePlot=TRUE){
   # t_i: year for sample i
 
   # Attach stuff
-  attach( Extrapolation_List )
-  on.exit( detach(Extrapolation_List) )
+  library( maps )
+  library( mapdata )
+  on.exit( detach(package:mapdata), add=TRUE )
+  on.exit( detach(package:maps), add=TRUE )
 
   # Initialize GM models
   model_O1 = RandomFields::RMgauss(var=Sim_Settings[['SigmaO1']]^2, scale=Sim_Settings[['Range1']])
@@ -21,10 +23,16 @@ function(Sim_Settings, Extrapolation_List, MakePlot=TRUE){
   model_E2 = RandomFields::RMgauss(var=Sim_Settings[['SigmaE2']]^2, scale=Sim_Settings[['Range2']])
 
   # Generate locations, years, vessel for each sample i
-  s_i = sample(1:nrow(Data_Extrap), size=Sim_Settings[['Nyears']]*Sim_Settings[['Nsamp_per_year']]) #, nrow=Sim_Settings[['Nsamp_per_year']], ncol=Sim_Settings[['Nyears']])
-  loc_i = Data_Extrap[s_i,c('E_km','N_km','Lat','Lon')]
+  s_i = sample(1:nrow(Extrapolation_List$Data_Extrap), size=Sim_Settings[['Nyears']]*Sim_Settings[['Nsamp_per_year']]) #, nrow=Sim_Settings[['Nsamp_per_year']], ncol=Sim_Settings[['Nyears']])
+  loc_i = Extrapolation_List$Data_Extrap[s_i,c('E_km','N_km','Lat','Lon')]
   t_i = rep( 1:Sim_Settings[['Nyears']], each=Sim_Settings[['Nsamp_per_year']])
   v_i = rep(rep( 1:4, each=Sim_Settings[['Nsamp_per_year']]/4), Sim_Settings[['Nyears']])
+
+  # Generate intercepts
+  exp_beta1_t = Sim_Settings[['beta1_mean']] + Sim_Settings[['beta1_slope']] * (1:Sim_Settings[['Nyears']]-mean(1:Sim_Settings[['Nyears']])/2)
+  beta1_t = rnorm(Sim_Settings[['Nyears']], mean=exp_beta1_t, sd=Sim_Settings[['beta1_sd']] )
+  exp_beta2_t = Sim_Settings[['beta2_mean']] + Sim_Settings[['beta2_slope']] * (1:Sim_Settings[['Nyears']]-mean(1:Sim_Settings[['Nyears']])/2)
+  beta2_t = rnorm(Sim_Settings[['Nyears']], mean=exp_beta2_t, sd=Sim_Settings[['beta2_sd']] )
 
   # Simulate random components
   O1_i = RandomFields::RFsimulate(model=model_O1, x=loc_i[,'E_km'], y=loc_i[,'N_km'])@data[,1] #), nrow=Sim_Settings[['Nsamp_per_year']], ncol=Sim_Settings[['Nyears']])
@@ -34,14 +42,14 @@ function(Sim_Settings, Extrapolation_List, MakePlot=TRUE){
     E1_i[which(t_i==t)] = RandomFields::RFsimulate(model=model_E1, x=loc_i[which(t_i==t),'E_km'], y=loc_i[which(t_i==t),'N_km'])@data[,1]
     E2_i[which(t_i==t)] = RandomFields::RFsimulate(model=model_E2, x=loc_i[which(t_i==t),'E_km'], y=loc_i[which(t_i==t),'N_km'])@data[,1]
   }
-  X_ij = as.matrix(Data_Extrap[s_i,c('Depth_km','Depth_km2','Rock_dist_')])
+  X_ij = as.matrix(Extrapolation_List$Data_Extrap[s_i,c('Depth_km','Depth_km2','Rock_dist_')])
   X_ij = ifelse( is.na(X_ij), outer(rep(1,nrow(X_ij)),colMeans(X_ij,na.rm=TRUE)), X_ij)
   Vessel_vyc = array( rnorm( n=4*Sim_Settings[['Nyears']]*2, mean=0, sd=Sim_Settings[['SigmaVY1']]), dim=c(4,Sim_Settings[['Nyears']],2))
 
   # Calculate expected values, and simulate
-  P1_i = O1_i + E1_i + as.matrix(X_ij)%*%unlist(Sim_Settings[c('Depth_km','Depth_km2','Dist_sqrtkm')])
+  P1_i = beta1_t[t_i] + O1_i + E1_i + as.matrix(X_ij)%*%unlist(Sim_Settings[c('Depth_km','Depth_km2','Dist_sqrtkm')])
   R1_i = plogis( P1_i + Vessel_vyc[v_i[which(t_i==t)],t,1] )
-  P2_i = O2_i + E2_i + as.matrix(X_ij)%*%unlist(Sim_Settings[c('Depth_km','Depth_km2','Dist_sqrtkm')])
+  P2_i = beta2_t[t_i] + O2_i + E2_i + as.matrix(X_ij)%*%unlist(Sim_Settings[c('Depth_km','Depth_km2','Dist_sqrtkm')])
   R2_i = exp( P2_i + Vessel_vyc[v_i[which(t_i==t)],t,2] )
   CPUE_i = rlnorm( n=length(R2_i), meanlog=log(R2_i), sdlog=Sim_Settings[['SigmaM']]) * rbinom( n=length(R1_i), size=1, prob=R1_i )
   Data_Geostat = cbind( "Catch_KG"=CPUE_i, "Year"=t_i, "Vessel"=v_i, "AreaSwept_km2"=1/1e2, "Lat"=loc_i[,'Lat'], "Lon"=loc_i[,'Lon'] )
@@ -53,9 +61,9 @@ function(Sim_Settings, Extrapolation_List, MakePlot=TRUE){
     Xlim = c(-126,-117); Ylim = c(32,49)
     MapSizeRatio = c( "Height(in)"=4, "Width(in)"=2 )
     for(RespI in 1:5){
-      Mat = matrix(NA, ncol=Sim_Settings[['Nyears']], nrow=nrow(Data_Extrap))
+      Mat = matrix(NA, ncol=Sim_Settings[['Nyears']], nrow=nrow(Extrapolation_List$Data_Extrap))
       for(t in 1:Sim_Settings[['Nyears']]){
-        NN_Extrap = nn2( data=loc_i[which(t_i==t),c('E_km','N_km')], query=Data_Extrap[,c('E_km','N_km')], k=1 )
+        NN_Extrap = RANN::nn2( data=loc_i[which(t_i==t),c('E_km','N_km')], query=Extrapolation_List$Data_Extrap[,c('E_km','N_km')], k=1 )
         if(RespI==1) Mat[,t] = (R1_i[which(t_i==t)])[NN_Extrap$nn.idx]
         if(RespI==2) Mat[,t] = (R2_i[which(t_i==t)])[NN_Extrap$nn.idx]
         if(RespI==3) Mat[,t] = (R1_i[which(t_i==t)]*R2_i[which(t_i==t)])[NN_Extrap$nn.idx]
@@ -67,16 +75,19 @@ function(Sim_Settings, Extrapolation_List, MakePlot=TRUE){
         par(mfrow=c(2,5), mar=c(2,2,0,0))
         for(t in 1:Sim_Settings[['Nyears']]){
           maps::map("worldHires", ylim=Ylim, xlim=Xlim, col="grey90",fill=T, main="", mar=c(0,0,2.5,0),interior=TRUE)
-          points(x=Data_Extrap[,'Lon'], y=Data_Extrap[,'Lat'], col=Col(n=10)[ceiling(f(Mat)[,t]*9)+1], cex=0.01)
+          points(x=Extrapolation_List$Data_Extrap[,'Lon'], y=Extrapolation_List$Data_Extrap[,'Lat'], col=Col(n=10)[ceiling(f(Mat)[,t]*9)+1], cex=0.01)
           #points( x=Data_Geostat[,'Lon'], y=Data_Geostat[,'Lat'], col=Col(n=10)[ceiling(f(Mat)[,t]*9)+1], cex=0.05)
         }
       dev.off()
       # Legend
       png(file=paste0(DateFile,"True_",switch(RespI, "Pres","Pos","Dens","Pos_Rescaled","Dens_Rescaled"),"_Legend.png",sep=""), width=1, height=2*MapSizeRatio['Height(in)'], res=200, units='in')
-        SpatialDeltaGLMM:::Heatmap_Legend( colvec=Col(n=50), heatrange=range(Mat), margintext=switch(RespI, "Encounter probability","log(Positive catch rate)",expression(paste("log Density, log(kg. / ",km^2,")",sep="")),NULL,NULL) )
+        SpatialDeltaGLMM:::Heatmap_Legend( colvec=Col(n=50), heatrange=range(Mat), textmargin=switch(RespI, "Encounter probability","log(Positive catch rate)",expression(paste("log Density, log(kg. / ",km^2,")",sep="")),NULL,NULL) )
       dev.off()
     }
   }
-  Return = list( "Data_Geostat"=Data_Geostat, "True_Index"=True_Index )
+
+  # Return stuff
+  Return = list( "Data_Geostat"=Data_Geostat )
+  if( MakePlot==TRUE ) Return[["True_Index"]] = True_Index
   return( Return )
 }
